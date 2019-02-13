18c033225c915695818d1a756760de073bc700a2
Daniel Yovchev <daniel@yovchev.io>
2019-02-13 09:48:03 +0000

    Github Pages example

---
+++ docs/js/about.13f9ca9f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About Us")]),n("p",[t._v("This page is loaded dynamicaly")])])}],l=n("2877"),u={},c=Object(l["a"])(u,a,s,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.13f9ca9f.js.map
